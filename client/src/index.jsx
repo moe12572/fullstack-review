69cea80999d195dce056796f92b01fee2a5847b8
Moe Chowdhury <moe.chowdhury@outlook.com>
2020-11-02 20:53:30 -0500

    Fixed bug in Search.jsx

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -7,22 +7,23 @@ import RepoList from './components/RepoList.jsx';
 class App extends React.Component {
   constructor(props) {
     super(props);
-    this.state = { 
+    this.state = {
       repos: []
     }
 
   }
 
-  search (term) {
+  search(term) {
+    console.log(this);
     console.log(`${term} was searched`);
     // TODO
   }
 
-  render () {
+  render() {
     return (<div>
       <h1>Github Fetcher</h1>
-      <RepoList repos={this.state.repos}/>
-      <Search onSearch={this.search.bind(this)}/>
+      <RepoList repos={this.state.repos} />
+      <Search onSearch={this.search.bind(this)} />
     </div>)
   }
 }
